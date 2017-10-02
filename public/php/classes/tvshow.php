3afc33c4a5a8dafd6e596a1654e34cff6b93752e
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-02 17:40:55 +0200

    Temporarily fixed css, added highlighting to currently airing show.

--- public/php/classes/tvshow.php
+++ public/php/classes/tvshow.php
@@ -1,13 +1,24 @@
 <?php
 class TvShow {
+	private $current_date;
+	private $start_date;
+
 	public $title;
 	public $start;
 	public $stop;
 
 	function __construct($title, $start, $stop) {
+		$this->current_date = date("Y-M-d");
+		$this->start_date = date("Y-M-d", $start);
+
 		$this->title      = $title;
 		$this->start      = date("H:i", $start);
 		$this->stop       = date("H:i", $stop);
+	}
+
+	function is_currently_airing() {
+		$now = date("H:i");
+		return $this->current_date === $this->start_date && $this->start < $now && $this->stop >= $now;
 	}
 
 	function to_string() {
--- public/php/index.php
+++ public/php/index.php
@@ -5,35 +5,41 @@ include './classes/tvshow.php';
 
 <div>
 	<?php
-	$date = isset($_GET["date"]) ? htmlspecialchars($_GET["date"]) : "2017-10-02"; # TODO
+	$date = isset($_GET["date"]) ? htmlspecialchars($_GET["date"]) : date('Y-m-d');
 	$url = "http://json.xmltv.se/svt1.svt.se_{$date}.js.gz";
 	$obj = json_decode(file_get_contents($url), true);
 	$programmes = $obj["jsontv"]["programme"];
 	$tv_shows = array();
+	?>
 
-	echo "<table class='header'>";
-	echo "<th>Starttid</th><th>Sluttid</th><th>Titel</th>";
-	echo "</table>";
-	echo "<table class='container'>";
+	<table>
+		<th>
+			Starttid
+		</th>
 
+		<th>
+			Sluttid
+		</th>
+
+		<th>
+			Titel
+		</th>
+
+	<?php
 	for ($i = 0; $i < sizeof($programmes); $i++) {
 		$title = $programmes[$i]["title"]["sv"];
 		$start = $programmes[$i]["start"];
 		$stop  = $programmes[$i]["stop"];
 		$tv_shows[$i] = new TvShow($title, $start, $stop);
 		
-		echo "<tr>";
+		echo $tv_shows[$i]->is_currently_airing() ? "<tr class='tr-currently-airing'>" : "<tr>";
 		echo "<td>" . $tv_shows[$i]->start . "</td>";
 		echo "<td>" . $tv_shows[$i]->stop . "</td>";
 		echo "<td>" . $tv_shows[$i]->title . "</td>";
 		echo "</tr>";		
 	}
-
 	echo "</table>";
-
-	foreach ($tv_shows as $show) {
-		#echo $show->to_string() . "<br />";
-	}
 	?>
 </div>
+
 <?php include 'footer.php'; ?>
