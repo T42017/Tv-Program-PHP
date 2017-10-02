a301c9531c96a08b62f935ec9714f3f2c1b0bdf3
pontuslaos <pontus.laos@lund.ntig.se>
2017-10-02 14:03:34 +0200

    CSS......

--- public/php/index.php
+++ public/php/index.php
@@ -5,30 +5,30 @@ include './classes/tvshow.php';
 
 <div>
 	<?php
-	if (!isset($_GET["date"])) {
-		$date = "2017-10-02"; // TODO
-	}
-	else {
-		$date = htmlspecialchars($_GET["date"]);
-	}
+	$date = isset($_GET["date"]) ? htmlspecialchars($_GET["date"]) : "2017-10-02"; # TODO
 	$url = "http://json.xmltv.se/svt1.svt.se_{$date}.js.gz";
 	$obj = json_decode(file_get_contents($url), true);
 	$programmes = $obj["jsontv"]["programme"];
 	$tv_shows = array();
 
-	echo "<table>";
+	echo "<table class='header'>";
 	echo "<th>Starttid</th><th>Sluttid</th><th>Titel</th>";
+	echo "</table>";
+	echo "<table class='container'>";
+
 	for ($i = 0; $i < sizeof($programmes); $i++) {
 		$title = $programmes[$i]["title"]["sv"];
 		$start = $programmes[$i]["start"];
 		$stop  = $programmes[$i]["stop"];
 		$tv_shows[$i] = new TvShow($title, $start, $stop);
+		
 		echo "<tr>";
 		echo "<td>" . $tv_shows[$i]->start . "</td>";
 		echo "<td>" . $tv_shows[$i]->stop . "</td>";
 		echo "<td>" . $tv_shows[$i]->title . "</td>";
-		echo "</tr>";
+		echo "</tr>";		
 	}
+
 	echo "</table>";
 
 	foreach ($tv_shows as $show) {
@@ -36,5 +36,4 @@ include './classes/tvshow.php';
 	}
 	?>
 </div>
-
 <?php include 'footer.php'; ?>
